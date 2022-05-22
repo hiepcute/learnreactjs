388c392be4ec42eaa2e1966f17384ac7e49d812c
DO VAN HIEP <39146260+hiepcute@users.noreply.github.com>
2022-05-22 21:39:51 +0700

    hiehie

--- src/Content.js
+++ src/Content.js
@@ -1,52 +1,75 @@
-import { useEffect ,useState} from "react"
+import { useEffect, useState } from "react"
 
 // 1 callback luoon duoc goi sau khi component mounted
-const tabs=['posts','comments','albums']
+const tabs = ['posts', 'comments', 'albums']
 
-function Content(){
+function Content() {
     // useeffect (callback, [deps])
-    const [title,setTitle] =useState('')
-    const [posts,setPosts] = useState([])
-    const [type,setType] = useState('posts')
-   
+    const [title, setTitle] = useState('')
+    const [posts, setPosts] = useState([])
+    const [type, setType] = useState('posts')
+    const [showGoToTop, setShowGoToTop] = useState(false)
   
-     useEffect(()=>{
+
+    useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/${type}`)
-        .then(res=>res.json())
-        .then(posts=>{
-           // setPosts(posts)
-        })
-       
-     },[type])
- 
-
-
-    return(
-    <div>
-        {tabs.map(tab=>(
-            <button
-            key={tab}
-            style={type===tab?{
-                color: '#fff',
-                backgroundColor: '#333',
-            }:{}}
-            onClick={()=>setType(tab)}
-            >
-                {tab}
-            </button>
-        ))}
-        <input
-        value={title}
-        onChange={e=>setTitle(e.target.value)}
-        />
-        <ul>
-            {posts.map(post=>(
-                <li key={post.id}>{post.title}</li>
-            ))}
-        </ul>
-
+            .then(res => res.json())
+            .then(posts => {
+                setPosts(posts)
+            })
         
-    </div>
+    }, [type])
+
+    useEffect(() => {
+        const handleScroll = () => {
+            
+            if (window.scrollY >= 200) {
+                setShowGoToTop(true)
+
+            } else {
+                setShowGoToTop(false)
+            }
+        }
+        window.addEventListener('scroll', handleScroll)
+        return ()=>{
+            window.removeEventListener('scroll', handleScroll)
+        }
+
+    }, [])
+
+    return (
+        <div>
+            {tabs.map(tab => (
+                <button
+                    key={tab}
+                    style={type === tab ? {
+                        color: '#fff',
+                        backgroundColor: '333',
+                    } : {}}
+                    onClick={() => setType(tab)}
+                >
+                    {tab}
+                </button>
+            ))}
+            <input
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+            />
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
+            {showGoToTop && (<button
+            style={{ position: 'fixed',
+            right: 20,
+            bottom:20,
+            color: '000'}}>
+                 go to Top
+            </button>
+
+            )}
+        </div>
     )
 }
 export default Content
